17a665593601b398136c029ea9ab97241817acb0
ElemelonWind <c.yang.2023@gmail.com>
2024-12-18 22:57:15 -0500

    better double digits

--- chimp.py
+++ chimp.py
@@ -22,22 +22,31 @@ pt.pytesseract.tesseract_cmd = r'C:\\Program Files\\Tesseract-OCR\\tesseract.exe
 gridinfo = [[0 for x in range(8)] for y in range(5)]
 for y in range(5):
     for x in range(8):
-        cropped = screenshot.crop((x * CELLWIDTH, y * CELLHEIGHT, (x+1)*CELLWIDTH, (y+1)*CELLHEIGHT))
+        # cropped = screenshot.crop((x * CELLWIDTH, y * CELLHEIGHT, (x+1)*CELLWIDTH, (y+1)*CELLHEIGHT))
+        cropped = cv.imread(f'cell_{x}_{y}.png')
         cell = np.array(cropped)
-        # cell = cv.imread(f'cell_{x}_{y}.png')
+        cell = cv.imread(f'cell_{x}_{y}.png')
         hsv = cv.cvtColor(cell, cv.COLOR_BGR2HSV)
         mask = cv.inRange(hsv, np.array([255, 255, 245]), np.array([255, 15, 255]))
         cell[mask>0] = (0, 0, 0)
         blur = cv.blur(cell, (5, 5))
         # if cell has a number, save it in gridinfo
-        data_1_char = pt.image_to_string(cell, lang='eng', config='--psm 8 -c tessedit_char_whitelist=0123456789', nice=0, output_type='string', timeout=5)
-        data_2_char = pt.image_to_string(cell, lang='eng', config='--psm 10 -c tessedit_char_whitelist=0123456789', nice=0, output_type='string', timeout=5)
+        data_1_char = pt.image_to_string(cell, lang='eng', config='--psm 10 -c tessedit_char_whitelist=0123456789', nice=0, output_type='string', timeout=5)
+        data_2_char = pt.image_to_string(cell, lang='eng', config='--psm 13 -c tessedit_char_whitelist=0123456789', nice=0, output_type='string', timeout=5)
+        data_3_char = pt.image_to_string(cell, lang='eng', config='--psm 8 -c tessedit_char_whitelist=0123456789', nice=0, output_type='string', timeout=5)
+        
         if data_1_char.strip().isdigit():
-            print(data_1_char)
+            # print("1", data_1_char)
             gridinfo[y][x] = int(data_1_char)
+            # print("1", data_1_char, data_2_char, data_3_char)
         elif data_2_char.strip().isdigit():
-            print(data_2_char)
+            # print("2", data_2_char)
             gridinfo[y][x] = int(data_2_char)
+            # print("2", data_1_char, data_2_char, data_3_char)
+        elif data_3_char.strip().isdigit():
+            # print("3", data_3_char)
+            gridinfo[y][x] = int(data_3_char)
+            # print("3", data_1_char, data_2_char, data_3_char)
 
 # find order to click
 order = sorted([(gridinfo[y][x], x, y) for y in range(5) for x in range(8) if gridinfo[y][x] > 0], key=lambda x: x[0])
@@ -46,3 +55,4 @@ for o in order:
 
 time.sleep(0.1)
 pyautogui.click(CONTINUEX, CONTINUEY)
+time.sleep(0.1)
